11b24b9302729e18724c7b1cb75003fae14984e3
Igor <igorlfs@ufmg.br>
2023-11-22 22:37:32 -0300

    finish subgraph ffs

--- subgraph.jl
+++ subgraph.jl
@@ -22,31 +22,53 @@ function main()
     num_vertices::Int64 = parse(Int64, split(readline(input_file), "\t")[2])
     adj_matrix::Matrix{Float64} = read_matrix(input_file, num_vertices)
 
-    vertices_weights = zeros(num_vertices)
+    vertices_weights = []
     for i in 1:num_vertices
-        vertices_weights[i] = sum([adj_matrix[i, j] for j in 1:num_vertices])
+        push!(vertices_weights, [sum([adj_matrix[i, j] for j in 1:num_vertices]), i])
     end
 
     removed = fill(false, num_vertices)
-    for (i, v) in enumerate(vertices_weights)
-        if v < 0 && !removed[i]
-            removed[i] = true
-            for j in 1:num_vertices
-                vertices_weights[j] -= adj_matrix[i, j]
+    while true
+        start = count(removed)
+
+        for i in 1:num_vertices
+            vertices_weights[i] = [sum([adj_matrix[i, j] for j in 1:num_vertices]), i]
+        end
+
+        sort!(vertices_weights)
+
+        for (vw, i) in vertices_weights
+            i = floor(Int, i)
+            if vw < 0
+                removed[i] = true
+                vertices_weights[i] = 0
+                for j in 1:num_vertices
+                    adj_matrix[j, i] = 0
+                    adj_matrix[i, j] = 0
+                end
+                break
             end
-            adj_matrix[i, :] .= 0
-            adj_matrix[:, i] .= 0
+        end
+
+        finish = count(removed)
+
+        if finish == start
+            break
         end
     end
 
-    cost = 0
-    for (i, v) in enumerate(removed)
-        if !v
-            cost += sum(adj_matrix[i, :])
+    cost = sum(i[1] for i in vertices_weights) / 2
+
+    println("TP2 $MATRÍCULA = $cost")
+
+    sort!(vertices_weights, by=x -> x[2])
+
+    for (_, v) in enumerate(vertices_weights)
+        if v[1] > 0
+            print("$(floor(Int,v[2]))\t")
         end
     end
-
-    println("TP2 $MATRÍCULA = $(cost/2)")
+    println()
 end
 
 main()
